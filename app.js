c71c2a2e218015998904266f52903481c450dd99
James-koduah <jameskoduah171@gmail.com>
2022-05-14 21:27:49 +0100

    initial files

---
+++ app.js
@@ -0,0 +1,7 @@
+const rr = document.querySelector("dis");
+const bteen = document.querySelector(".test");
+
+bteen.addEventListener("click", changeColor);
+  function changeColor(){
+    rr.style.color = 'blue';
+  }
