2a07c556d2a085e6faafa85f142a36d968d6529c
James-koduah <jameskoduah171@gmail.com>
2022-06-11 12:18:17 +0100

    Added the caps lock and space functions.
    Added the functions that would enable caps Lock to change the input text to UpperCase and added space to give space

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const rr = document.querySelector("dis");
-const bteen = document.querySelector(".test");
 
-bteen.addEventListener("click", changeColor);
-  function changeColor(){
-    rr.style.color = 'blue';
+  let caps=false;
+let hh=document.getElementById('aaa')
+function func(e){
+  let elem = e.innerHTML.toLowerCase();
+  if(caps===true){elem = e.innerHTML.toUpperCase();}
+  if(caps===false){elem=e.innerHTML.toLowerCase();}
+
+  if(e.innerHTML=='Caps Lock'){
+    if(caps===true){
+      e.style.backgroundColor='rgb(11, 163, 11)'
+      return caps=false
+    }else{
+      e.style.backgroundColor ='white'
+      return caps=true
+    }
+  }else
+  if(e.innerHTML=='space'){
+    hh.innerHTML+=' '
   }
+  else{hh.innerHTML+=elem;}
+  
+  
+  console.log(caps)
+}
